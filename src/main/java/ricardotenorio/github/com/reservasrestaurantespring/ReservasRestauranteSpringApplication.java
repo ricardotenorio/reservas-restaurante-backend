08b6b5317a30fc9e9941323ea6c30ad96a433458
ricardotenorio <ricardo.heber@gmail.com>
2021-07-02 00:12:15 -0300

    first commit

---
+++ src/main/java/ricardotenorio/github/com/reservasrestaurantespring/ReservasRestauranteSpringApplication.java
@@ -0,0 +1,13 @@
+package ricardotenorio.github.com.reservasrestaurantespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservasRestauranteSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservasRestauranteSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ricardotenorio/github/com/reservasrestaurantespring/ReservasRestauranteSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package ricardotenorio.github.com.reservasrestaurantespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservasRestauranteSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
